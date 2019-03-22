ecf5820210afd434ccc72a7705a0ecafc14e39c2
Timothy <sysop@timb.us>
2019-03-22 08:02:35 -0400

    Changed autovenv_* flags from '1' to 'yes'

--- init.fish
+++ init.fish
@@ -1,17 +1,17 @@
 if status is-interactive
     test -z "$autovenv_announce"
-        and set -g autovenv_announce "1"
+        and set -g autovenv_announce "yes"
     test -z "$autovenv"
-        and set -g autovenv_enable "1"
+        and set -g autovenv_enable "yes"
 
     function autovenv --on-variable PWD -d "Automatic activation of Python virtual enviroments"
-        test ! "$autovenv_enable" = "1"
+        test ! "$autovenv_enable" = "yes"
             and return
         for autovenv_find_dir in (string split -n '/' "$PWD")
             set autovenv_find_tree "$autovenv_find_tree/$autovenv_find_dir"
             if test -e "$autovenv_find_tree/bin/activate.fish"
                 set autovenv_source "$autovenv_find_tree/bin/activate.fish"
-                if test "$autovenv_announce" = "1"
+                if test "$autovenv_announce" = "yes"
                     set autovenv_name_old "$autovenv_name"
                     set -g autovenv_name (basename "$autovenv_find_tree")
                 end
@@ -20,14 +20,14 @@ if status is-interactive
         end
         if test -z "$VIRTUAL_ENV" -a -e "$autovenv_source"
             source "$autovenv_source"
-            if test "$autovenv_announce" = "1"
+            if test "$autovenv_announce" = "yes"
                 echo "Activated Virtual Enviroment ($autovenv_name)"
             end
         else if test -n "$VIRTUAL_ENV"
             set autovenv_dir (string match -n "$VIRTUAL_ENV*" "$PWD")
             if test -z "$autovenv_dir" -a ! -e "$autovenv_source"
                 deactivate
-                if test "$autovenv_announce" = "1"
+                if test "$autovenv_announce" = "yes"
                     echo "Deactivated Virtual Enviroment ($autovenv_name)"
                     set -e autovenv_name
                     set -e autovenv_name_old
@@ -35,7 +35,7 @@ if status is-interactive
             else if test -z "$autovenv_dir" -a -e "$autovenv_source"
                 deactivate
                 source "$autovenv_source"
-                if test "$autovenv_announce" = "1"
+                if test "$autovenv_announce" = "yes"
                     echo "Switched Virtual Enviroments ($autovenv_name_old => $autovenv_name)"
                 end
             end
